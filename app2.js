94c7c10f0876641914932b15f43aa8173ad143f1
Glauber Santiago <glaubersantiago@outlook.com>
2025-02-11 19:38:30 -0300

    REVISÃO DOM

--- app2.js
+++ app2.js
@@ -1,5 +1,26 @@
-let nome = document.querySelector("#teste")
+// Seletor de id.
+// let nome = document.querySelector("#teste")
 
-nome.innerHTML = `
-    <h1>Olá Mundo!</h1>
-`
+// Seletor de tag.
+let nome2 = document.querySelector("h1")
+
+// Inserir HTML utilizando javascript.
+// nome.innerHTML = `
+//     <h1>Olá Mundo!</h1>
+// `
+
+// Inserir texto utilizando javascript.
+nome2.innerText = "O marcão é o brabo que vai pagar uma pizza pra todo mundo."
+
+// nome2.classList.remove("oiSom")
+
+function changeColorToRed() {
+    let classe = nome2.className // função que pega o que tá na classe.
+    if(classe === "teste2") {
+        //Inserir estilo utilizando javascript.
+        nome2.classList.remove("teste2")
+    } else {
+        // Inserir estilo utilizando javascript.
+        nome2.classList.add("teste2")
+    }
+}
